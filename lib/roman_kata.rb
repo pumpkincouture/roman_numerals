9b466c1c1728929b58e8b1e235c921f99a11dd5e
pumpkincouture <pumpkincouture@gmail.com>
2014-08-19 14:59:48 -0500

    Changes 8-19

--- lib/roman_kata.rb
+++ lib/roman_kata.rb
@@ -3,32 +3,37 @@ class RomanNumerals
   attr_reader :numerals_hash
 
 	def initialize
-	  @numerals_hash = {1 => "I", 5 => "V", 
+	  @numerals_hash = {1 => "I", 4 => "IV", 5 => "V", 9 => "IX",
 									10 => "X", 50 => "L", 100 => "C",
 									500 => "D", 1000 => "M"}
 	end
 
-	def find_base(number)
+	def convert(number)
 
-	  @numerals_hash.each do |k,v|
-			if k == number
-			return v
+	  @numerals_hash.each do |arabic, roman|
+			if arabic == number
+			  return roman
+			elsif number >= 10
+				difference = number - 10
+					if arabic == difference
+						equivalent = "X" + roman
+						return equivalent
+						# equiv = "X" + roman 
+						# return equiv
+					# elsif arabic != difference
+					# 	equiv = "X" + ("I" * difference)
+					# 	return equiv
+					end
+			elsif number >= 10
+				equivalent = "X" + ("I" * difference)
+			elsif number >= 5
+					difference = number - 5
+			    equiv = "V" + ("I" * difference)
+			    return equiv
+			elsif number <= 3
+		     equiv = "I" * number	
+			   return equiv
 			end
 		end
-		return number
-	end
-
-	def find_equivalent(number)
-		first_try = find_base(number)
-
-		if first_try.is_a? String
-			return false
-		elsif number <=3
-			equiv = "I" * number
-			return equiv
-		else
-			return number
-		end
 	end	
-
 end
--- spec/kata_spec.rb
+++ spec/kata_spec.rb
@@ -7,19 +7,76 @@ describe RomanNumerals do
 	end
 
 	it "initializes with a hash." do
-	  numerals_hash = {1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M"}
+	  numerals_hash = {1 => "I", 2 => "II", 3 => "III", 4 => "IV", 
+	  								5 => "V", 6 => "VI", 7 => "VII", 
+	  								8 => "VIII", 9 => "IX", 10 => "X", 
+	  								50 => "L", 100 => "C",
+										500 => "D", 1000 => "M"}
 		expect(@start_convert.numerals_hash).to eq(numerals_hash)
 	end
 
-	it "takes a number as an argument and finds the equivalent within the numerals hash" do
-	  numerals_hash = {1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M"}
+	it "returns equivalent of 10" do
 	  result = "X"
-	  expect(@start_convert.find_base(10)).to eq(result)
+	  expect(@start_convert.convert(10)).to eq(result)
 	end
 
-	it "tries to find the equivalent of the number that the first method did not find" do
-		numerals_hash = {1 => "I", 5 => "V", 10 => "X", 50 => "L", 100 => "C", 500 => "D", 1000 => "M"}
-		result = "III" 
-		expect(@start_convert.find_equivalent(3)).to eq(result)
+	it "returns equivalent of 1" do
+		result = "I" 
+		expect(@start_convert.convert(1)).to eq(result)
+	end
+
+	it "returns equivalent of 2" do
+		result = "II"
+		expect(@start_convert.convert(2)).to eq(result)
+	end
+
+	it "returns equivalent of 3" do
+		result = "III"
+		expect(@start_convert.convert(3)).to eq(result)
+	end
+
+	it "returns equivalent of 4" do
+		result = "IV"
+		expect(@start_convert.convert(4)).to eq(result)
+	end
+
+	it "returns equivalent of 5" do
+		result = "V"
+		expect(@start_convert.convert(5)).to eq(result)
+	end
+
+	it "returns equivalent of 6" do
+		result = "VI"
+		expect(@start_convert.convert(6)).to eq(result)
+	end
+
+	it "returns roman equivalent of 7" do
+		result = "VII"
+		expect(@start_convert.convert(7)).to eq(result)
+	end
+
+	it "returns roman equivalent of 8" do
+		result = "VIII"
+		expect(@start_convert.convert(8)).to eq(result)
+	end
+
+	it "returns roman equivalent of 12" do
+		result = "XII"
+		expect(@start_convert.convert(12)).to eq(result)
+	end
+
+	it "returns roman equivalent of 13" do
+		result = "XIII"
+		expect(@start_convert.convert(13)).to eq(result)
+	end
+
+	it "returns roman equivalent of 14" do
+		result = "XIV"
+		expect(@start_convert.convert(14)).to eq(result)
+	end
+
+	it "returns roman equivalent of 15" do
+		result = "XV"
+		expect(@start_convert.convert(15)).to eq(result)
 	end
 end
