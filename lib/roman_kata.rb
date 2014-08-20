a45ee4f54eca23c857a269492e8707f7229db953
pumpkincouture <pumpkincouture@gmail.com>
2014-08-20 09:32:22 -0500

    Changes 8-20

--- lib/roman_kata.rb
+++ lib/roman_kata.rb
@@ -3,37 +3,39 @@ class RomanNumerals
   attr_reader :numerals_hash
 
 	def initialize
-	  @numerals_hash = {1 => "I", 4 => "IV", 5 => "V", 9 => "IX",
-									10 => "X", 50 => "L", 100 => "C",
-									500 => "D", 1000 => "M"}
+	  @numerals_hash = {1 => "I", 2 => "II", 3 => "III", 4 => "IV", 
+	  									5 => "V", 6 => "VI", 7 => "VII", 
+	  									8 => "VIII", 9 => "IX", 10 => "X", 
+	  									40 => "XL", 50 => "L", 100 => "C",
+											500 => "D", 1000 => "M"}
 	end
 
 	def convert(number)
 
 	  @numerals_hash.each do |arabic, roman|
+	  	equivalent = ""
+	  	difference = number - 10
+	  	last_numeral = difference - 10
+			ten_divisible = number - last_numeral
+
+			ten_base = ten_divisible/10 
+
 			if arabic == number
 			  return roman
-			elsif number >= 10
-				difference = number - 10
-					if arabic == difference
-						equivalent = "X" + roman
+			else
+				if arabic == difference
+						equivalent += "X"
+						equivalent += roman
 						return equivalent
-						# equiv = "X" + roman 
-						# return equiv
-					# elsif arabic != difference
-					# 	equiv = "X" + ("I" * difference)
-					# 	return equiv
-					end
-			elsif number >= 10
-				equivalent = "X" + ("I" * difference)
-			elsif number >= 5
-					difference = number - 5
-			    equiv = "V" + ("I" * difference)
-			    return equiv
-			elsif number <= 3
-		     equiv = "I" * number	
-			   return equiv
+				elsif arabic == last_numeral
+						equivalent += "X" * ten_base 
+						equivalent += roman
+						return equivalent
+				# elsif arabic == ten_divisible
+				# 		equivalent += roman
+				# 		equivalent += "V"
+				end
 			end
-		end
-	end	
+	  end	
+  end
 end
--- spec/kata_spec.rb
+++ spec/kata_spec.rb
@@ -9,7 +9,7 @@ describe RomanNumerals do
 	it "initializes with a hash." do
 	  numerals_hash = {1 => "I", 2 => "II", 3 => "III", 4 => "IV", 
 	  								5 => "V", 6 => "VI", 7 => "VII", 
-	  								8 => "VIII", 9 => "IX", 10 => "X", 
+	  								8 => "VIII", 9 => "IX", 10 => "X", 40 => "XL", 
 	  								50 => "L", 100 => "C",
 										500 => "D", 1000 => "M"}
 		expect(@start_convert.numerals_hash).to eq(numerals_hash)
@@ -78,5 +78,55 @@ describe RomanNumerals do
 	it "returns roman equivalent of 15" do
 		result = "XV"
 		expect(@start_convert.convert(15)).to eq(result)
+	end
+
+	it "returns roman equivalent of 19" do
+		result = "XIX"
+		expect(@start_convert.convert(19)).to eq(result)
+	end
+
+	it "returns roman equivalent of 20" do
+		result = "XX"
+		expect(@start_convert.convert(20)).to eq(result)
+	end
+
+	it "returns roman equivalent of 22" do
+		result = "XXII"
+		expect(@start_convert.convert(22)).to eq(result)
+	end
+
+	it "returns roman equivalent of 23" do
+		result = "XXIII"
+		expect(@start_convert.convert(23)).to eq(result)
+	end
+
+	it "returns roman equivalent of 24" do
+		result = "XXIV"
+		expect(@start_convert.convert(24)).to eq(result)
+	end
+
+	it "returns roman equivalent of 25" do
+		result = "XXV"
+		expect(@start_convert.convert(25)).to eq(result)
+	end
+
+	it "returns roman equivalent of 30" do
+		result = "XXX"
+		expect(@start_convert.convert(30)).to eq(result)
+	end
+
+	it "returns roman equivant of 35" do
+		result = "XXV"
+		expect(@start_convert.convert(35)).to eq(result)
+	end
+	
+	it "returns roman equivalent of 40" do
+		result = "XL"
+		expect(@start_convert.convert(40)).to eq(result)
+	end
+
+	it "returns roman equivalent of 45" do
+		result = "XLV"
+		expect(@start_convert.convert(45)).to eq(result)
 	end
 end
