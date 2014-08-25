2078b70cbfa21e854cc839b90a9a3d34a4edd43b
pumpkincouture <pumpkincouture@gmail.com>
2014-08-25 09:50:28 -0500

    Final

--- lib/roman_kata.rb
+++ lib/roman_kata.rb
@@ -1,26 +1,15 @@
 class RomanNumerals
-  def convert(number)
-  	roman = ""
-  	bases = { 1000 => "M", 900 => "CM", 500 => "D", 100 => "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV" }
 
-  		bases.each do |arabic, base|
-  			while number >= arabic
-  				# if number == 29
-  				# 	roman = "XXIX"
-  				# 	return roman 
-  				# elsif number == 19
-  				# 	roman = "XIX"
-  				# 	return roman
-  				# elsif number == 9
-  				# 	roman = "IX"
-  				# 	return roman
-  				# else
-					roman += base
-					number -= arabic
-					# end
-				end
-  		end
- 			roman += ("I" * number) 
-   roman
+  def convert(number)
+    roman = ""
+    roman_hash = {1000 => "M", 900 => "CM", 500 => "D", 400 => "CD", 100 => "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
+
+    roman_hash.each do |arabic, base|
+      while number >= arabic
+        roman += base
+        number -= arabic
+      end
+    end
+    roman
   end
 end
--- spec/kata_spec.rb
+++ spec/kata_spec.rb
@@ -6,104 +6,78 @@ describe RomanNumerals do
 
   describe "#convert" do
 
-	  before :each do
-		  @roman_numerals = RomanNumerals.new
-		end
+  	before :each do
+  		@roman_numerals = RomanNumerals.new
+  	end
 
-		it "Returns I when passed 1" do
-			expect(@roman_numerals.convert(1)).to eq("I")
-		end
+ it "returns I" do
+ 	expect(@roman_numerals.convert(1)).to eq("I")
+ end
 
-		it "Returns II when passed 2" do
-			expect(@roman_numerals.convert(2)).to eq("II")
-		end
+ it "returns II" do
+ 	expect(@roman_numerals.convert(2)).to eq("II")
+ end
 
-		it "Returns III when passed 3" do
-			expect(@roman_numerals.convert(3)).to eq("III")
-		end
+ it "returns III" do
+ 	expect(@roman_numerals.convert(3)).to eq("III")
+ end
 
-		it "Returns IV when passed 4" do
-			expect(@roman_numerals.convert(4)).to eq("IV")
-		end
+ it "returns IV" do
+ 	expect(@roman_numerals.convert(4)).to eq("IV")
+ end
 
-		it "Returns V when passed 5" do
-			expect(@roman_numerals.convert(5)).to eq("V")
-		end
+ it "returns V" do
+ 	expect(@roman_numerals.convert(5)).to eq("V")
+ end
 
-		it "Returns VI when passed 6" do
-			expect(@roman_numerals.convert(6)).to eq("VI")
-		end
+ it "returns VI" do
+ 	expect(@roman_numerals.convert(6)).to eq("VI")
+ end
 
-		it "Returns VII when passed 7" do
-			expect(@roman_numerals.convert(7)).to eq("VII")
-		end
+ it "returns IX" do
+ 	expect(@roman_numerals.convert(9)).to eq("IX")
+ end
 
-		it "Returns VIII when passed 8" do
-			expect(@roman_numerals.convert(8)).to eq("VIII")
-		end
+ it "returns X" do
+ 	expect(@roman_numerals.convert(10)).to eq("X")
+ end
 
-		it "Returns IX when passed 9" do
-			expect(@roman_numerals.convert(9)).to eq("IX")
-		end
+ it "returns XL" do
+ 	expect(@roman_numerals.convert(40)).to eq("XL")
+ end
 
-		it "Returns X when passed 10" do
-			expect(@roman_numerals.convert(10)).to eq("X")
-		end
+ it "returns L" do
+ 	expect(@roman_numerals.convert(50)).to eq("L")
+ end
 
-		it "Returns XI when passed 11" do
-			expect(@roman_numerals.convert(11)).to eq("XI")
-		end
+ it "returns XC" do
+ 	expect(@roman_numerals.convert(90)).to eq("XC")
+ end
 
-		it "Returns XII when passed 12" do
-			expect(@roman_numerals.convert(12)).to eq("XII")
-		end
+ it "returns C" do
+ 	expect(@roman_numerals.convert(100)).to eq("C")
+ end
 
-		it "Returns XIII when passed 13" do
-			expect(@roman_numerals.convert(13)).to eq("XIII")
-		end
+ it "returns CD" do
+ 	expect(@roman_numerals.convert(400)).to eq("CD")
+ end
 
-		it "Returns XIV when passed 14" do
-			expect(@roman_numerals.convert(14)).to eq("XIV")
-		end
+ it "returns D" do
+ 	expect(@roman_numerals.convert(500)).to eq("D")
+ end
 
-		it "Returns XV when passed 15" do
-			expect(@roman_numerals.convert(15)).to eq("XV")
-		end
+ it "returns CM" do
+ 	expect(@roman_numerals.convert(900)).to eq("CM")
+ end
 
-		it "Returns XIX when passed 19" do
-			expect(@roman_numerals.convert(19)).to eq("XIX")
-		end
+ it "returns M" do
+ 	expect(@roman_numerals.convert(1000)).to eq("M")
+ end
 
-		it "Returns XX when passed 20" do
-			expect(@roman_numerals.convert(20)).to eq("XX")
-		end
-
-		it "Returns XXI when passed 21" do
-			expect(@roman_numerals.convert(21)).to eq("XXI")
-		end
-
-		it "Returns XXIV when passed 24" do
-			expect(@roman_numerals.convert(24)).to eq("XXIV")
-		end
-
-		it "Returns XXV when passed 25" do
-			expect(@roman_numerals.convert(25)).to eq("XXV")
-		end
-
-		it "Returns XXIX when passed 29" do
-			expect(@roman_numerals.convert(29)).to eq("XXIX")
-		end
-
-		it "Returns XL when passed 40" do
-			expect(@roman_numerals.convert(40)).to eq("XL")
-		end
-
-		it "Returns XLV when passed 45" do
-			expect(@roman_numerals.convert(45)).to eq("XLV")
-		end
-
-		it "Returns MCMXCIX when passed 1999" do
-			expect(@roman_numerals.convert(1999)).to eq("MCMXCIX")
-		end
-	end
+ it "returns MMM" do
+ 	expect(@roman_numerals.convert(3000)).to eq("MMM")
+ end
 end
+end
+
+		
